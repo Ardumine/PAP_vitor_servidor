469e98811acfda1afb2de5c6db6aea559ab0e6d8
Ardumine <david.codeco@gmail.com>
2023-06-19 13:50:58 +0100

    Server update

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-// https server
+console.log("A iniciar...");
+
 const https = require("https");
   
 // Express for handling GET and POST request
 const express = require("express");
 const app = express();
   
-// Requiring file system to use local files
 const fs = require("fs");
   
 
   
-// Get request for root of the app
 app.get("*", function (req, res) {
     console.log("." + req.path);
   res.sendFile(__dirname + req.path);
@@ -19,17 +18,15 @@ app.get("*", function (req, res) {
 
     
   
-  
-// Creating object of key and certificate
-// for SSL
+ 
 const options = {
   key: fs.readFileSync("server.key"),
   cert: fs.readFileSync("server.cert"),
+  host: "*"
 };
   
-// Creating https server by passing
-// options and app object
+
 https.createServer(options, app)
-.listen(5500, function (req, res) {
-  console.log("Server started at port 5500");
+.listen(5501, function (req, res) {
+  console.log("Servidor iniciado!");
 });
